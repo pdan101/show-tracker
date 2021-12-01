75fb6d2e50fbbc9b00ea735f5e7e2b326880db9e
Prithwish Dan <dan.prithwish@gmail.com>
2021-11-30 21:37:15 -0500

    Added login and images

--- backend_api/src/db.py
+++ backend_api/src/db.py
@@ -1,6 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+import requests
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
+
+load_dotenv()
+key = os.getenv('KEY')
+base_url = 'https://pixabay.com/api/'
 
 
 class Show(db.Model):
@@ -13,6 +20,7 @@ class Show(db.Model):
     genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"), nullable=False)
     is_plan_to_watch = db.Column(db.Boolean, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    image_url = db.Column(db.String, nullable=True)
 
     def __init__(self, **kwargs):
         self.name = kwargs.get("name")
@@ -22,6 +30,13 @@ class Show(db.Model):
         self.genre_id = kwargs.get("genre_id")
         self.is_plan_to_watch = kwargs.get("is_plan_to_watch")
         self.user_id = kwargs.get("user_id")
+        payload = {'key': key,
+                   'q': "+".join(self.name.split()), 'image_type': "photo"}
+        r = requests.get(base_url, params=payload)
+        try:
+            self.image_url = r.json().get('hits')[0].get('largeImageURL')
+        except Exception as e:
+            self.image_url = 'https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-image-available-icon-flat-vector.jpg'
 
     def serialize(self):
         if not self.is_plan_to_watch:
@@ -31,14 +46,16 @@ class Show(db.Model):
                 "genre": Genre.query.filter_by(id=self.genre_id).first().name,
                 "year_released": self.year_released,
                 "start_date": self.start_date,
-                "finished": self.finished
+                "finished": self.finished,
+                "image_url": self.image_url
             }
         else:
             return {
                 "id": self.id,
                 "name": self.name,
                 "genre": Genre.query.filter_by(id=self.genre_id).first().name,
-                "year_released": self.year_released
+                "year_released": self.year_released,
+                "image_url": self.image_url
             }
 
     def serialize_watch(self):
@@ -48,7 +65,8 @@ class Show(db.Model):
             "genre": Genre.query.filter_by(id=self.genre_id).first().name,
             "year_released": self.year_released,
             "start_date": self.start_date,
-            "finished": self.finished
+            "finished": self.finished,
+            "image_url": self.image_url
         }
 
     def serialize_plan(self):
@@ -56,7 +74,8 @@ class Show(db.Model):
             "id": self.id,
             "name": self.name,
             "genre": Genre.query.filter_by(id=self.genre_id).first().name,
-            "year_released": self.year_released
+            "year_released": self.year_released,
+            "image_url": self.image_url
         }
 
 
